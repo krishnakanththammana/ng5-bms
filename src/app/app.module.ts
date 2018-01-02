13df9bc01b82787a3a3e3f0646a987acbe23581a
krishnakanththammana <krishnakanththammana@gmail.com>
2018-01-02 07:04:48 +0530

    movie card component now has dynamic temp data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatTabsModule, MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponentComponent } from './home-component/home-component.component';
@@ -21,6 +21,7 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
     MatButtonModule,
     MatCheckboxModule,
     MatTabsModule,
+    MatCardModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent implements OnInit {
-
+  people: any[] = [
+    {
+      'name': 'Douglas  Pace'
+    },
+    {
+      'name': 'Mcleod  Mueller'
+    },
+    {
+      'name': 'Day  Meyers'
+    },
+    {
+      'name': 'Aguirre  Ellis'
+    },
+    {
+      'name': 'Cook  Tyson'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
